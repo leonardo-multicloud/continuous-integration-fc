941a74207a8da4c2b45b67cae98dd8a5cdbcb41a
leonardo-multicloud <leonardo.multicloud@gmail.com>
2022-12-26 16:43:37 -0300

    Teste

--- math_test.go
+++ math_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(15, 15)
+	total := Soma(11, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado inválido: Resultado: %d. Esperado: %d", total, 30)
